79418f1fca826af70d097289db6a672c09614198
iti-0204 <84443576+iti-0204@users.noreply.github.com@users.noreply.github.com>
2024-05-29 05:42:05 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -14,16 +14,23 @@ class ListsController < ApplicationController
   end
 
   def index
-    @lists = List.all  
+    @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])  
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-  
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   private
   # ストロングパラメータ
   def list_params
